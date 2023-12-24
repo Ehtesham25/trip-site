b4a81000f12bd0399c0855aacf6302178638dbb9
Ehtesham Khan <72070084+Ehtesham25@users.noreply.github.com>
2023-12-24 00:47:01 -0800

    Update authReducer.js

--- src/reducers/authReducer.js
+++ src/reducers/authReducer.js
@@ -3,7 +3,7 @@ import { getId } from "../actionTypes/ActionTypes";
 export default (state = [{ id: [] }], action) => {
   switch (action.type) {
     case getId.GET_ID:
-      return [...state, action.payload];
+      return {...state, action.payload};
     default:
       return state;
   }
