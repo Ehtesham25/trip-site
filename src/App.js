cadf4ba6d6518d81696e1b533bb1d4addaa38460
Ehtesham Khan <72070084+Ehtesham25@users.noreply.github.com>
2025-12-01 05:49:58 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
-
+ 
 import { getAllTrips } from "../src/actions/TripActions"; 
 import "./App.css";
 
